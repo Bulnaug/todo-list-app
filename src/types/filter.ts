e942404a16bfbb3300b8785ee53d69b408a91b74
Bulnaug <basalay.1997@mail.ru>
2026-01-02 18:34:05 +0100

    fixed enum issue fir vercel

--- src/types/filter.ts
+++ src/types/filter.ts
@@ -1,5 +1,7 @@
-export enum Filter {
-  ALL = 'all',
-  ACTIVE = 'active',
-  COMPLETED = 'completed',
-}
+export const Filter = {
+  ALL: 'all',
+  ACTIVE: 'active',
+  COMPLETED: 'completed',
+} as const;
+
+export type Filter = typeof Filter[keyof typeof Filter];
